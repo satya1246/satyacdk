5bf4d962a03839ad84a468d395ff57768a2e8712
Ubuntu <ubuntu@ip-172-31-23-235.ec2.internal>
2024-07-05 22:45:38 +0000

    Initial commit

---
+++ src/Satyatest/SatyatestStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace Satyatest
+{
+    public class SatyatestStack : Stack
+    {
+        internal SatyatestStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
